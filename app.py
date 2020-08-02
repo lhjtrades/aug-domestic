9f76604db4d4522e2430460a3f337178acb5e67f
lhjtrades <lhjtrades@gmail.com>
2020-08-02 20:46:05 +0000

    first change

--- app.py
+++ app.py
@@ -3,44 +3,41 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
+from datetime import datetime
+import os
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.config['SECRET'] = os.getenv("SECRET_KEY")
+SECRET = app.config['SECRET']
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'joiners'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = f'mongodb+srv://admin_2:{SECRET}@cluster0.fnifx.mongodb.net/joiners?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return render_template('index.html', events = events)
+    return render_template("index.html", time = datetime.now())
 
-
-# CONNECT TO DB, ADD DATA
-
-@app.route('/add')
-
-def add():
-    # connect to the database
-
-    # insert new data
-
-    # return a message to the user
-    return ""
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        joiners = mongo.db.joiners
+        existing_joiner = joiners.find_one({'username' : request.form['search']})
+        info = dict(existing_joiner)
+        info2 = existing_joiner['items']
+        if existing_joiner is None:
+            return render_template('index.html', time = datetime.now())
+        return render_template('index.html', info = info, info2 = info2, time = datetime.now())
+    return render_template('index.html', time = datetime.now())
