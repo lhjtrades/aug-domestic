08d2da804c40ef9b8d22c8e02c829ddd7916832a
lhjtrades <lhjtrades@gmail.com>
2020-08-11 23:47:19 +0000

    5

--- app.py
+++ app.py
@@ -35,16 +35,14 @@ def index():
 def search():
     if request.method == 'POST':
         joiners = mongo.db.joiners
-        existing_joiner = joiners.find({})
-    return render_template('index.html')
-    #     info = dict(existing_joiner)
-    #     info2 = existing_joiner['items']
-    #     if existing_joiner is None:
-    #         return render_template('index.html', time = datetime.now())
-    #     else:
-    #         return render_template('index.html', info = info, info2 = info2, time = datetime.now())
-    # else:
-    #     return render_template('index.html', time = datetime.now())
+        existing_joiner = joiners.find({'username':'lhjtrades'})
+        if existing_joiner is None:
+            return render_template('index.html', time = datetime.now())
+        else:
+            info = dict(existing_joiner)
+            info2 = existing_joiner['items']
+            return render_template('index.html', info = info, info2 = info2, time = datetime.now())
+    return render_template('index.html', time = datetime.now())
 
 @app.route('/test')
 def test():
