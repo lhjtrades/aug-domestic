b8ac030f0fdf5d180af67ac812acde7009212283
lhjtrades <lhjtrades@gmail.com>
2020-08-12 01:59:08 +0000

    convert code

--- app.py
+++ app.py
@@ -23,6 +23,39 @@ app.config['MONGO_URI'] = f'mongodb+srv://simple_user:QQVQXGLwg3ACixff@cluster0.
 
 mongo = PyMongo(app)
 
+# DICTIONARY
+item_keywords = {
+    "DT1":"Dome Tour Trading Cards (B1)",
+    "FFCarat": "Fallin Flower Carat ver. Photocards",
+    "DTPola1": "Dome Tour Polaroid Cards (White ver) + Unit Cards",
+    "HMV1": "Heng:garae HMVs (B1)",
+    "Haru1": "Haru Trading Cards (B1)",
+    "HMV2": "Heng:garae HMVs (B2)",
+    "Carrot1": "Ideal Cut Japan Carrot Cards (B1)",
+    "DTHolo1": "Dome Tour Holographic Cards (B1)",
+    "3FS2": "AppleMusic/MusicArt Fansign Photocards (B2)",
+    "4FS2": "Soundwave/M2U Fansign Photocards (B2)",
+    "DTHolo2": "Dome Tour Holographic Cards (B2)",
+    "2FS":"Synnara/Hottracks Fansign Photocards",
+    "FFReg":"Fallin Flower Regular ver. Photocards",
+    "DTPola2":"Dome Tour Polaroid Cards (white ver)",
+    "Carrot2":"Ideal Cut Japan Carrot Cards (B2)",
+    "OTYPola":"Ode to You Japan Polaroid Cards",
+    "DTPolaColor":"Dome Tour Polaroid Cards (color/sign ver)",
+    "OTYLen":"Ode to You Japan Lenticular Cards",
+    "DTHolo3":"Dome Tour Holographic Cards (B3)",
+    "3FS3":"AppleMusic/MusicArt Fansign Photocards (B3)",
+    "4FS3":"Soundwave/M2U Fansign Photocards (B3)",
+    "Haru2":"Haru Trading Cards (B2)",
+    "1FS": "Yes24/Interpark Fansign Photocards",
+    "DT2":"Dome Tour Trading Cards (B2)",
+    "4FS1":"Soundwave/M2U Fansign Photocards (B1)",
+    "OTY":"Ode to You Japan Trading Cards",
+    "FFJosh":"Fallin Flower Joshua Photocards",
+    "HGFansign":"Album + Fansign PC GO",
+    "personalorder":"Personal Order"
+}
+
 # -- Routes section --
 # INDEX
 
@@ -41,7 +74,7 @@ def search():
             wow = list(existing_joiner)
             info = wow[0]
             info2 = info['items']
-            return render_template('index.html', info = info, info2 = info2, time = datetime.now())
+            return render_template('index.html', info = info, info2 = info2, time = datetime.now(), item_key = item_keywords)
         else:
             error = "That username doesn't exist. Please try again or contact me if you think there is a mistake."
             return render_template('index.html', error = error)
